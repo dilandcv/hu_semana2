28599b5fd51cc895e4fd06d76ca9ab8688d78160
Josevar-99 <josemiguelvargas_@hotmail.com>
2026-03-25 07:30:53 -0500

    adelanto de inventario

---
+++ Menu.py
@@ -0,0 +1,13 @@
+def mostrar_menu():
+    opcion = "4"
+    while opcion == "4":
+        print("""BIENVENIDO AL MENU
+        1:agregar producto 
+        2:Mostrar inventario 
+        3:calcular estadisticas
+        4: salir""")
+        opcion =input("insert a option: ")
+
+        if opcion == "1":
+            
+mostrar_menu()
